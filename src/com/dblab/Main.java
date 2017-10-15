7e67feba94ba7a4d3df38ce9e88a838f47f9d1f5
Farzad <farzadjafari1996@gmail.com>
2017-10-15 19:15:03 +0330

    2nd session

--- src/com/dblab/Main.java
+++ src/com/dblab/Main.java
@@ -1,8 +1,69 @@
 package com.dblab;
 
+import com.mysql.cj.api.jdbc.JdbcConnection;
+import com.pengrad.telegrambot.Callback;
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.GetUpdates;
+import com.pengrad.telegrambot.response.GetUpdatesResponse;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.*;
+import com.mysql.cj.jdbc.Driver;
+
+import java.io.IOException;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        TelegramBot bot = new TelegramBot("444383849:AAHL2WwNZrGYuZDu3jGORLP5kbSOSJWzO2I");
+        GetUpdates getUpdates = new GetUpdates().limit(100).offset(0).timeout(0);
+        JdbcLogin jdbcLogin = new JdbcLogin();
+        Connection connection = jdbcLogin.StartBdd();
+        // async
+        bot.execute(getUpdates, new Callback<GetUpdates, GetUpdatesResponse>() {
+            @Override
+            public void onResponse(GetUpdates request, GetUpdatesResponse response) {
+                List<Update> updates = response.updates();
+                for (Update update : updates) {
+                    try{
+                        connection.createStatement().execute("insert into base(txt) values (\""+ update.message().text()+"\")");
+                    }catch (SQLException e){e.printStackTrace();};
+                }
+            }
+
+            @Override
+            public void onFailure(GetUpdates request, IOException e) {
+
+            }
+        });
+    }
+
+    public static class JdbcLogin {
+        public String Login;
+        public String MotDePasse;
+        private boolean Logged = false;
+
+        public Connection StartBdd(){
+            String driverName = "com.mysql.jdbc.Driver";
+            try{
+                Class.forName(driverName); // here is the ClassNotFoundException
+            }catch (ClassNotFoundException e){e.printStackTrace();}
+
+            String serverName = "localhost";
+            String mydatabase = "digiLab";
+            String url = "jdbc:mysql://" + serverName + "/" + mydatabase;
+
+            String username = "farzad";
+            String password = "12345";
+            try{
+                Connection connection = DriverManager.getConnection(url, username, password);
+                return connection;
+            }catch (SQLException e){e.printStackTrace();}
+            return null;
+        }
     }
 }
